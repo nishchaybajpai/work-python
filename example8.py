22e07313c0f275d219fdf164b38493696c319929
nishchaybajpai <106794876+nishchaybajpai@users.noreply.github.com>
2022-06-16 19:55:45 +0530

    Create example8.py

---
+++ example8.py
@@ -0,0 +1,12 @@
+from turtle import *
+s = Screen()
+s.setup(400,400)
+s.bgcolor('green')
+
+pencolor('white')
+for i in range(5):
+    lt(72)
+    for i in range(5):
+         fd(100)
+         rt(144)
+mainloop()
